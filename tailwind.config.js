8114ba40eaf4606e10ebafb9ff8f882bef39b2cd
Sakib2435 <viasakib381@gmail.com>
2024-10-20 02:41:07 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap'); 
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
